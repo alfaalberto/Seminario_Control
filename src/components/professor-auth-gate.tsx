44ad3138fb19d52a69a652bacfe24d6f1873bece
Alberto Rosales <arosaless23@gmail.com>
2025-07-20 23:10:36 +0000

    depura todo este proyecto y elimina todos los archivos que no se estén u

--- src/components/professor-auth-gate.tsx
+++
@@ -1,4 +0,0 @@
-// This file is no longer needed as authentication is now handled globally.
-// I will leave it empty to avoid build errors from other files that might import it.
-// In a real project, this file would be deleted.
-export {};
--- src/lib/firestore.ts
+++
@@ -1,4 +0,0 @@
-// This file is intentionally left blank to disable Firestore seeding.
-// The application will use local mock data provided in /src/lib/data.ts
-// To re-enable Firestore, this file and the hooks would need to be updated.
-export {};
